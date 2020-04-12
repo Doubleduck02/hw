5122868acc640c069eeab0b986731542ee9e241d
Doubleduck02 <danielchu4@gmail.com>
2020-04-12 20:14:17 +0200

    other files

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-print(ascii('│'))
-print(ascii('─'))
-print(ascii('┼'))
-print('\u2500'*3)
+#print(ascii('│'))
+#print(ascii('─'))
+#print(ascii('┼'))
+#print('\u2500'*3)
+from random import randint
+def check5(x):
+    print(x)
+    if x==5:
+        return True
+    return False
+
+#while not check5(randint(1,5)):
+    #pass
+#print("it's %s"%('5')) 
+print(f'{randint(1,10):02}')
