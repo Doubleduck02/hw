9d0ac4a16e462a146c46205102b183eedc77ec48
Doubleduck02 <danielchu4@gmail.com>
2020-05-25 13:42:57 +0200

    update on progress
    forgot to push changes

--- .vscode/test.py
+++ .vscode/test.py
@@ -2,3 +2,13 @@ print(ascii('│'))
 print(ascii('─'))
 print(ascii('┼'))
 print('\u2500'*3)
+print(ascii('└'))
+print(ascii('┴'))
+print(ascii('┘'))
+tab=[]
+x=[' ']
+x.append(' ')
+x.append(' ')
+for i in range(3):
+    tab.append(x)
+print(tab)
---
+++ connect-4/c4.py
@@ -0,0 +1,100 @@
+from random import randint
+
+class Game:
+    def __init__(self, players=1, p1sign='o', p2sign='x'):
+        if players<1:
+            self.players=1
+        elif players>2:
+            self.players=2
+        self._board=[]
+        if len(p1sign)!=1:
+            p1sign='o'
+        if len(p2sign)!=1:
+            p2sign='x'
+        self._p1sign=p1sign
+        self._p2sign=p2sign
+
+    def boardset(self, x_size=7,y_size=6):
+        x=[]
+        for i in range(x_size):
+            x.append(' ')
+        for i in range(y_size):
+            self._board.append(x)
+        return True
+
+    def playersign(self, player):
+        if player==1:
+            return self._p1sign
+        elif player==2:
+            return self._p2sign
+        return ' '
+
+    def sign(self, x, y):
+        return self._board[y][x]
+
+    def draw(self):
+        s='  1   2   3   4   5   6   7\n'
+        for y in range(6):
+            s=s+' '
+            for x in range(7):
+                if x==6:
+                    s=s+' '+self.sign(x,y)+'\n'
+                else:
+                    s=s+' '+self.sign(x,y)+' \u2502'
+            if y<5:
+                s=s+' '+('\u2500'*3+'\u253c')*6+'\u2500'*3+'\n'
+        return s
+
+    def wincheck(self):
+        for y in range(6):
+            for x in range(7):
+                if self.sign(x,y)==' ':
+                    continue
+                if x-4>=0:
+                    for i in range(4):
+                        if self.sign(x,y)!=self.sign(x-i,y):
+                            break
+                        if i==3:
+                            return True
+                    if y-4>=0:
+                        for i in range(4):
+                            if self.sign(x,y)!=self.sign(x-i,y-i):
+                                break
+                            if i==3:
+                                return True
+                    if y+4<6:
+                        for i in range(4):
+                            if self.sign(x,y)!=self.sign(x-i,y+i):
+                                break
+                            if i==3:
+                                return True
+                if x+4<7:
+                    for i in range(4):
+                        if self.sign(x,y)!=self.sign(x+i,y):
+                            break
+                        if i==3:
+                            return True
+                    if y-4>=0:
+                        for i in range(4):
+                            if self.sign(x,y)!=self.sign(x+i,y-i):
+                                break
+                            if i==3:
+                                return True
+                    if y+4<6:
+                        for i in range(4):
+                            if self.sign(x,y)!=self.sign(x+i,y+i):
+                                break
+                            if i==3:
+                                return True
+                if y-4>=0:
+                    for i in range(4):
+                        if self.sign(x,y)!=self.sign(x,y-i):
+                            break
+                        if i==3:
+                            return True
+                if y+4<6:
+                    for i in range(4):
+                        if self.sign(x,y)!=self.sign(x,y+i):
+                            break
+                        if i==3:
+                            return True
