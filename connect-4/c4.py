afc329d9e00b606ddad0c306424fce7f19ae241b
Doubleduck02 <danielchu4@gmail.com>
2020-06-05 16:19:46 +0200

    Update c4.py

--- connect-4/c4.py
+++ connect-4/c4.py
@@ -1,5 +1,10 @@
 from random import randint
 
+def check1to7(x):
+    if x in range(1,8):
+        return True
+    return False
+
 class Game:
     def __init__(self, players=1, p1sign='o', p2sign='x'):
         if players<1:
@@ -104,8 +109,34 @@ class Game:
     def move(self, player, x):
         for i in range(6):
             if self.sign(x,i)!=' ':
-                
-            
+                if player==0:
+                    self._board[i][x]=' '
+                    return True
+                elif i!=0:
+                    self._board[i-1][x]=self.playersign(player)
+                    return True
+                return False
+    
+    def botmove(self):
+        for x in range(7):
+            if self.move(1,x):
+                if self.wincheck():
+                    self.move(0,x)
+                    self.move(2,x)
+                    return True
+                self.move(0,x)
+        self.move(2,randint(0,6))
+        return True
+
+    def playermove(self, player=1):
+        while True:
+            xinput=input('Column: ')
+            if check1to7(xinput):
+                x=int(xinput)
+                if x in range(1,4) and y in range(1,4):
+                    if self.move(player,x-1):
+                        return True
+            print('Please choose again')
 
 
 game=Game()
